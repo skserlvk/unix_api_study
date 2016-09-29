5c6eb2c156b6d16cc85995381ed6be8a4fe49ad7
kunlv <308796727@qq.com>
2016-09-29 18:14:54 +0800

    Create popen_study.c

---
+++ popen_study.c
@@ -0,0 +1,16 @@
+/*
+#include <stdio.h>
+
+FILE * popen(const char *command , const char *type );
+int pclose(FILE *stream);
+
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
