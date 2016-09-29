6b4c68ca2b2b8841865b2084bd0f4006283cc716
kunlv <308796727@qq.com>
2016-09-29 18:24:12 +0800

    Update popen_study.c
    popen, if param type is 'r', return fp can be read ,if 'w', can be write.

--- popen_study.c
+++ popen_study.c
@@ -11,6 +11,22 @@ int pclose(FILE *stream);
 
 int main(int argc, char *argv[])
 {
+	char buf[128] = {0};
+	FILE *fp;
 
+	if (argc != 2 || argv[1] == NULL) {
+		printf("Invalid param\n");
+		return -1;
+	}
+	
+	/* type: r, fp can be read. */
+	fp = popen(argv[1], "r");
+	
+	printf("popen call [ %s ] execute result :\n");
+	while (fgets(buf, sizeof(buf), fp) != NULL)
+		printf(">> %s", buf);
+	
+	fclose(fp);
+	
 	return 0;
 }
